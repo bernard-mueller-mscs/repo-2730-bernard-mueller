1c81f815e32e24c70a17e46f56e61c0725c98211
Bernie Mueller <Bernie Mueller>
2017-08-22 19:34:01 -0500

    Add project files.

---
+++ bmueller1b1/Splash form.vb
@@ -0,0 +1,17 @@
+﻿Public Class frmSplash
+	Private Sub lblMsg_Click(sender As Object, e As EventArgs)
+
+	End Sub
+
+	Private Sub lblName_Click(sender As Object, e As EventArgs) Handles lblName.Click
+
+	End Sub
+
+	Private Sub picStorks_Click(sender As Object, e As EventArgs) Handles picStorks.Click
+
+	End Sub
+
+	Private Sub tmrExit4_Tick(sender As Object, e As EventArgs) Handles tmrExit4.Tick
+		Me.Close()
+	End Sub
+End Class
